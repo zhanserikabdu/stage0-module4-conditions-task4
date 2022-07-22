cbb687df03d990b2fee8dfd8fb16a1aec2f29868
NANO <File changed>
2022-07-23 01:09:26 +0600

    2 submission

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
         int result;
-        result = (first > second) ? 10 : -10;
+        result = (first > second) ? first : second;
         System.out.println(result);
     }
 }
